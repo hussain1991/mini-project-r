67e09f860ac4a664f3ec36df9d8afc1186589f3c
Mohammed Hussain <39586440+hussain1991@users.noreply.github.com>
2018-05-26 20:39:46 +0530

    Solved

--- Moneyball.R
+++ Moneyball.R
@@ -40,3 +40,34 @@ rem_players <- subset(combo,!playerID %in% c('giambja01','damonjo01',
                                              'saenzol01'))
 rem_players <- subset(rem_players, yearID==2001)
 rem_players <- select(rem_players,playerID,salary,HR,OBP,AB)
+
+
+## we need to select 3 replacements for the lost players from the remaining players 
+#3Conditions are: 
+#The total combined salary of the three players can not exceed 15 million dollars.
+#Their combined number of At Bats (AB) needs to be equal to or greater than the lost players.
+#Their mean OBP had to equal to or greater than the mean OBP of the lost players
+#This corresposnds to having sum of AB > 1469 and sum of OBP> 1.091
+
+
+#First let us plot the salaries to see how they look like
+
+library(ggplot2)
+ggplot(rem_players,aes(x=OBP,y=salary)) + geom_point(aes(color=AB))
+
+#From the graph we can see there are many players who have an OBP of at least 0.25 and have salary less than
+#7.5 million and also have an AB greater than 500. These would be our ideal players
+
+rem_players <- filter(rem_players,salary<7500000,OBP>0.25,AB>500)
+
+#arranging the players in descending order of OBP,AB and increasing order of Salary, we get
+buy_list <- arrange(rem_players,salary,desc(OBP),desc(AB))
+head(buy_list,10)
+
+#It is recommended to buy the following players at their salary:
+#pujolal01 200000
+#eckstda01 200000
+#mientdo01 215000
+
+#Of course, Depending on the availiability of players and budget, it is recommended to buy players from the given list.
+buy_list
